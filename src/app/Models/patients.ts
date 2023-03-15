13921de6b5de08da6ca8ef5cf192400694671866
Radwa Elgammal <elgammalr74@gmail.com>
2023-03-16 00:33:54 +0200

    add patient class

---
+++ src/app/Models/patients.ts
@@ -0,0 +1,10 @@
+export class Patients {
+    constructor (
+        public status:string,
+        public history:string,
+        public height:number,
+        public weight:number,
+        public hasInsurance:boolean,
+        public phone:number,
+        ){}
+}
