a2b6b2c2d9d8217410ef10b6ae958d4eecc26c84
Maha Elgendy <mahaelgende@gmail.com>
2023-04-05 00:56:16 +0200

    handle auth in appointment

--- src/app/Components/appointment/appointment-list/appointment-list.component.ts
+++ src/app/Components/appointment/appointment-list/appointment-list.component.ts
@@ -10,13 +10,20 @@ import { AppointmentService } from 'src/app/Services/appointment.service';
 })
 export class AppointmentListComponent {
   appointments:Appointment[]=[];
+  role : string| null;
+  isEmployeeOrPatient: boolean=false;
 
   constructor(public appointmentService:AppointmentService){
+    this.role = sessionStorage.getItem('role');
   }
   ngOnInit(){
+    if(this.role == 'employee' || this.role =="patient"){
+      this.isEmployeeOrPatient=true;
+    }
     this.appointmentService.getAllAppointments().subscribe(data=>{
       this.appointments = data;
       console.log(this.appointments);
     })
+    
   }
 }
